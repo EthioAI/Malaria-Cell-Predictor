665e273c75fc93cf7daf2ae13be2a692660d6409
Abdella Solomon <abdesoltak@gmail.com>
2022-08-22 14:51:43 +0300

    add cnn

--- attrs.py
+++ attrs.py
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import enum
-=======
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
 import os
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pandas as pd
 import cv2
 from skimage import color
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import StratifiedKFold
-<<<<<<< HEAD
 from sklearn.base import BaseEstimator, TransformerMixin, clone
-=======
 from sklearn.base import clone
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
 import multiprocessing
 
 
@@ -34,39 +24,35 @@ def dataset_loader():
     for i in os.listdir("data/cell_images/Parasitized"):
         if ".png" in i:
             path = "data/cell_images/Parasitized/"+i 
-<<<<<<< HEAD
+
             img = cv2.resize(plt.imread(path), (50,50))
             # img = img.reshape(-1, 3)
             X.append(img)
             y.append(1.)
-=======
+
             img = cv2.resize(plt.imread(path), (25,25))
             img = color.rgb2gray(img).ravel()
             X.append(img)
             y.append(1)
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
+
 
     for i in os.listdir("data/cell_images/Uninfected/"):
         if ".png" in i:
-            path = "data/cell_images/Uninfected/"+i
-<<<<<<< HEAD
+
             img = cv2.resize(plt.imread(path), (50,50))
             # img = img.reshape(-1, 3)
             X.append(img)
             y.append(0.)
-=======
+
             img = cv2.resize(plt.imread(path), (25,25))
             img = color.rgb2gray(img).ravel()
             X.append(img)
             y.append(0)
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
 
     X = np.array(X) 
     y = np.array(y)
 
     return X, y
-<<<<<<< HEAD
-
 
 class ArrayRavel(BaseEstimator, TransformerMixin):
     '''
@@ -81,8 +67,6 @@ class ArrayRavel(BaseEstimator, TransformerMixin):
     def transform(self, X:np.ndarray):
         X_raveled = np.array([x.ravel() for x in X])
         return X_raveled
-=======
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
     
 
 def cross_val_score(clf, X, y, n):
@@ -103,12 +87,11 @@ def cross_val_score(clf, X, y, n):
 
     return np.array(scores)
 
-<<<<<<< HEAD
 def display_cross_val_score(scores):
     print("Scores: ", scores)
     print("Mean: ", scores.mean())
     print("Standard deviation: ", scores.std())
-=======
+
 def display_cross_val_score(scores, write_log=False, write_path="cross_val_score.log"):
     mean = scores.mean()
     std = scores.std()
@@ -122,9 +105,3 @@ def display_cross_val_score(scores, write_log=False, write_path="cross_val_score
         with open(write_path, "w+") as f:
             f.write(output)
             f.close()
-
-
-
-
-
->>>>>>> 53d1c7ad4d536ba8a8b9005fda0fbb2e2be07d0f
